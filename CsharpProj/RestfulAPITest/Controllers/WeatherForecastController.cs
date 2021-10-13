bd1105f35da86b22fe69a4633c9650c5a77c0981
TCX <1505205884@qq.com>
2021-10-13 10:51:04 +0800

    service privides API

---
+++ CsharpProj/RestfulAPITest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestfulAPITest.Models;
+
+namespace RestfulAPITest.Controllers
+{
+    [ApiController]
+    [Route("book/add")]    
+    public class ValuesController : ControllerBase
+    {
+        private static Dictionary<string, Addrequest> DB = new Dictionary<string, Addrequest>();
+        [HttpPost]
+        public Addresponse Post([FromBody] Addrequest req)
+        {
+            Addresponse resp = new Addresponse();
+            try
+            {
+                DB.Add(req.ISBN, req);
+                resp.ISBN = req.ISBN;
+                resp.message = "交易成功";
+                resp.result = "S";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                resp.ISBN = "";
+                resp.message = "交易失败";
+                resp.result = "F";
+            }
+            return resp;
+        }
+
+    }
+}
---
+++ CsharpProj/RestfulAPITest/Models/AddModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulAPITest.Models
+{
+    public class Addrequest
+    {
+        public string ISBN { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public string date { get; set; }
+        public Author[] authors { get; set; }
+    }
+    public class Author
+    {
+        public string name { get; set; }
+        public string sex { get; set; }
+        public string brithday { get; set; }
+    }
+    public class Addresponse
+    {
+        public string result { get; set; }
+        public string message { get; set; }
+        public string ISBN { get; set; }
+    }
+}
